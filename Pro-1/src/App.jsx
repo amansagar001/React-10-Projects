108c3c54af0577bca913d9c2ed78174596c66351
aman kumar sagar <01amansagar@gmail.com>
2024-02-26 18:56:51 +0530

    pro-1 added

---
+++ Pro-1/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Hero from "./components/Hero";
+import Navi from "./components/navi";
+const App = () => {
+  return (
+    <>
+      <div>
+        <Navi />
+        <Hero />
+      </div>
+    </>
+  );
+};
+export default App;
